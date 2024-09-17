1386b293b7bae72af2916dd4e934fc2e6afd5382
heosmond <osmondhe@gmail.com>
2024-09-17 11:43:31 -0300

    initial

---
+++ routes/contacts.js
@@ -0,0 +1,37 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.send('Contacts route');
+});
+
+// Get all contacts
+router.get('/all', (req, res) => {
+    res.send('All contacts');
+});
+
+// Get a contact by id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('Contact by id ' + id);
+});
+
+router.post('/create', (req, res) => {
+    res.send('create record');
+})
+
+router.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('update record ' + id);
+})
+
+router.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('delete record ' + id);
+})
+
+export default router;
+
+  
