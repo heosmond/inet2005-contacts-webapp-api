0c50d603a6bbea722b6f2177b38d319af00cea9d
heosmond <osmondhe@gmail.com>
2024-09-24 10:54:43 -0300

    Added Multer and Prisma DB

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,80 @@
 import express from 'express';
+import multer from 'multer';
+import { PrismaClient } from '@prisma/client';
+    
+
 
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
-    res.send('Contacts route');
+// Multer setup
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/'); // save uploaded files in `public/images` folder
+    },
+    filename: function (req, file, cb) {
+        const ext = file.originalname.split('.').pop(); // get file extension
+        const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext; // generate unique filename - current timestamp + random number between 0 and 1000.
+        cb(null, uniqueFilename);
+    }
 });
+const upload = multer({ storage: storage });
+    
+//NEW PRISMA OBJECT
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+});
+    
 
 // Get all contacts
-router.get('/all', (req, res) => {
-    res.send('All contacts');
+router.get('/all', async (req, res) => {
+    const contacts = await prisma.contact.findMany();
+    res.json(contacts);
 });
 
 // Get a contact by id
-router.get('/:id', (req, res) => {
+router.get('/get/:id', async (req, res) => {
     const id = req.params.id;
-    res.send('Contact by id ' + id);
+
+    //to-do verify id is a number
+    if (isNaN(id)){
+        res.send('Provide a valid number.').status(400);
+        return;
+    }
+
+    const contact = await prisma.contact.findUnique({
+        where: {
+            id: parseInt(id),
+        },
+    });
 });
 
-router.post('/create', (req, res) => {
-    res.send('create record');
-})
+//MULTER file and text upload
+router.post('/create', upload.single('image'), async (req, res) => {
+
+    const { firstName, lastName, phone, email, title } = req.body;
+    const filename = req.file ? req.file.filename : null;
+
+    const contact = await prisma.contact.create({
+        data: {
+            firstName: firstName,
+            lastName: lastName,
+            title: title,
+            phone: phone,
+            email: email,
+            filename: filename
+        }
+    });
+
+    res.json(contact);
+
+});
 
 router.put('/update/:id', (req, res) => {
     const id = req.params.id;
-    res.send('update record ' + id);
+    
+    if(req.file){
+        console.log("File uploaded ", + req.file.filename);
+    }
 })
 
 router.delete('/delete/:id', (req, res) => {
